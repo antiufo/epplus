a4ff87c10284ed28a6f7e6c436c7962b627f8d05
Andrea Martinelli <martinelli-andrea@outlook.com>
2015-09-07 23:38:11 +0200

    fixed some errors

---
+++ EPPlus/Shaman.EPPlus/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfficeOpenXml
+{
+    internal static class ExtensionMethods
+    {
+        public static void Close(this Stream s)
+        {
+            s.Dispose();
+        }
+    }
+}
